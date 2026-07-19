a0179688eb5512855fab34e10dc0947ca8932564
Ram Shukla <codewithrex@gmail.com>
2026-07-19 19:56:03 +0530

    implementing sentry for error tracking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as Sentry from "@sentry/react";
 import "@/index.css";
 import App from "@/App";
+
+Sentry.init({
+  dsn: "https://d7e59d6fdb5bd135d53f920c1fcb0005@o4511222664724480.ingest.us.sentry.io/4511222675800064",
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: []
+  }
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
